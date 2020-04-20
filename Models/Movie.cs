7905c7ab3c566e6218d39938976359815c037aa4
Steven Green <sesoccer911@aol.com>
2020-04-20 16:36:37 -0700

    Implimenting the database with test data

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -8,6 +9,7 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; } //primary key for the DB
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")] //cleaning up the presentation of the data
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }

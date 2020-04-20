71634c2f7570cef7a6ca3b6baf17925ffeb46c06
Steven Green <sesoccer911@aol.com>
2020-04-20 16:49:39 -0700

    Cleaned up the presentation of the application and tested concurrency with the SQL DB

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -13,6 +13,8 @@ namespace RazorPagesMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")] //clearning up the presentation of the data, mapping the price value in the db
         public decimal Price { get; set; }
     }
 }
